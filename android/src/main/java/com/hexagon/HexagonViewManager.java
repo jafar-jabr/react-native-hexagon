34f9f9d874cf505e4ee375bc8b8c721cb198502a
Jafar jabr <jafar.jabr@punct.ro>
2022-12-02 09:54:24 +0200

    build: android hotreload fix

--- android/src/main/java/com/hexagon/HexagonViewManager.java
+++ android/src/main/java/com/hexagon/HexagonViewManager.java
@@ -1,9 +1,7 @@
 package com.hexagon;
 
-import android.graphics.Canvas;
 import android.graphics.Color;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
+
 import com.facebook.react.uimanager.SimpleViewManager;
 import com.facebook.react.uimanager.ThemedReactContext;
 import com.facebook.react.uimanager.annotations.ReactProp;
@@ -11,9 +9,6 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Handler;
 import android.util.Log;
-import android.widget.ImageView;
-
-
 import androidx.annotation.NonNull;
 
 import java.net.URL;
@@ -23,8 +18,12 @@ public class HexagonViewManager extends SimpleViewManager<EffectiveShapeView> {
   private  int cornerRadius=0;
   private String borderColor;
   private int borderWidth=0;
+  private int width=0;
+  private String src=null;
+  private int height=0;
+  private EffectiveShapeView reactImageView;
   private interface ImgStartListener {
-    void startLoading(String imgUrl);
+    void startLoading();
   }
 
   @Override
@@ -36,26 +35,29 @@ public class HexagonViewManager extends SimpleViewManager<EffectiveShapeView> {
   @Override
   @NonNull
   public EffectiveShapeView createViewInstance(ThemedReactContext reactContext) {
-    final EffectiveShapeView reactImageView = new EffectiveShapeView(reactContext);
+    reactImageView = new EffectiveShapeView(reactContext);
     final Handler handler = new Handler();
     imgStartListener = new ImgStartListener() {
       @Override
-      public void startLoading(final String imgUrl) {
-        startDownloading(imgUrl, handler, reactImageView);
+      public void startLoading() {
+        startDownloading( handler, reactImageView);
 
       }
     };
 
+    imgStartListener.startLoading();
     return reactImageView;
   }
-  private void startDownloading(final String imgUrl, final Handler handler, final EffectiveShapeView reactImageView) {
+  private void startDownloading( final Handler handler, final EffectiveShapeView reactImageView) {
     new Thread(new Runnable() {
       @Override
       public void run() {
         try {
-          URL url = new URL(imgUrl);
-          final Bitmap bmp = BitmapFactory.decodeStream(url.openConnection().getInputStream());
-          setImage(bmp, handler, reactImageView);
+          if(src!=null){
+            URL url = new URL(src);
+            final Bitmap bmp = BitmapFactory.decodeStream(url.openConnection().getInputStream());
+            setImage(bmp, handler, reactImageView);
+          }
         } catch (Exception e) {
           Log.e("ReactImageManager", "Error : " + e.getMessage());
         }
@@ -63,46 +65,33 @@ public class HexagonViewManager extends SimpleViewManager<EffectiveShapeView> {
     }).start();
   }
   private void setImage(final Bitmap bmp, Handler handler, final EffectiveShapeView reactImageView) {
-    handler.post(new Runnable() {
-      @Override
-      public void run() {
-
-        reactImageView.setImageBitmap(bmp);
-        reactImageView.setBorderWidth(borderWidth);
-        reactImageView.setBorderColor(Color.parseColor(borderColor));
-        reactImageView.drawShape(cornerRadius);
-        if(reactImageView.getLayoutParams() != null) {
-          reactImageView.getLayoutParams().height = 150;
-          reactImageView.getLayoutParams().width = 150;
-          reactImageView.setScaleType(ImageView.ScaleType.FIT_XY);
-          reactImageView.requestLayout();
-        }
-      }
-    });
-  }
-
-
-  public static Bitmap drawableToBitmap (Drawable drawable) {
-    if (drawable instanceof BitmapDrawable) {
-      return ((BitmapDrawable)drawable).getBitmap();
+    reactImageView.setImageBitmap(bmp);
+    reactImageView.setBorderWidth(borderWidth);
+    if(borderColor!=null){
+      reactImageView.setBorderColor(Color.parseColor(borderColor));
     }
-
-    Bitmap bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
-    Canvas canvas = new Canvas(bitmap);
-    drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
-    drawable.draw(canvas);
-
-    return bitmap;
+    reactImageView.drawShape(cornerRadius);
+    if(reactImageView.getLayoutParams() != null) {
+      android.view.ViewGroup.LayoutParams layoutParams = reactImageView.getLayoutParams();
+      layoutParams.width = width;
+      layoutParams.height = height;
+      reactImageView.setLayoutParams(layoutParams);
+    }
+    reactImageView.invalidate();
   }
+
   @ReactProp(name = "src")
   public void setSrc(EffectiveShapeView view, String uri) {
-    imgStartListener.startLoading(uri);
-  }
+    src=uri;
+    imgStartListener.startLoading();
+    view.invalidate();
 
+  }
 
   @ReactProp(name = "cornerRadius")
   public void setCornerRadius(EffectiveShapeView view, int _cornerRadius) {
     cornerRadius=_cornerRadius;
+    view.invalidate();
 
   }
 
@@ -113,9 +102,33 @@ public class HexagonViewManager extends SimpleViewManager<EffectiveShapeView> {
     }
 
     borderWidth= _borderWidth;
+    view.invalidate();
+
   }
+
   @ReactProp(name = "borderColor")
   public void setBorderColor(EffectiveShapeView view, String _borderColor) {
     borderColor=_borderColor;
+    view.invalidate();
+
+  }
+
+  @ReactProp(name = "width")
+  public void setWidth(EffectiveShapeView view, int _width) {
+    width=_width;
+    view.invalidate();
+  }
+
+  @ReactProp(name = "height")
+  public void setHeight(EffectiveShapeView view, int _height) {
+    height=_height;
+    view.invalidate();
+
+    if(view.getLayoutParams() != null) {
+      android.view.ViewGroup.LayoutParams layoutParams = view.getLayoutParams();
+      layoutParams.width = _height;
+      layoutParams.height = _height;
+      view.setLayoutParams(layoutParams);
+    }
   }
 }
